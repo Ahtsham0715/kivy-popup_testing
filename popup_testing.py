5112ca13a167b5f3251264b9382952022dc25e87
Ahtsham0715 <ahtsham50743@gmail.com>
2022-08-05 15:43:17 +0500

    first commit

---
+++ popup_testing.py
@@ -0,0 +1,77 @@
+from kivy.app import App
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.popup import Popup
+from kivy.uix.widget import Widget
+# from kivy.uix.boxlayout import BoxLayout
+from kivy.lang import Builder
+
+Builder.load_file('popup_testing.kv')
+
+# class MyLayout(Widget):
+#     def __init__(self, **kwargs):
+#         super(MyLayout, self).__init__(**kwargs)
+#     text = StringProperty('')
+#     @staticmethod
+#     def btn():
+#         show_popup()
+
+#     def label_overwrite(self, overwrite):
+#         print(f'text: {overwrite}')
+#         # self.ids.label_1.text = overwrite
+#         # self.ids['label_1'].text = str(overwrite)
+#         show = MyLayout()
+#         self.ids['label_1'].text = 'hello'
+        
+
+# class P(FloatLayout):
+
+#     def press_button(self):
+#         print('Button pressed')
+#         text = self.ids.input_1.text
+#         MyLayout.label_overwrite(self, text)
+
+# class MyApp(App):
+#     def build(self):
+#         return MyLayout()
+
+# def show_popup():
+#     show = P()
+#     popupWindow = Popup(title='PopupWindow', content=show, size_hint=(.6, .3), auto_dismiss=True)
+#     popupWindow.open()
+
+# if __name__ == '__main__':
+#     MyApp().run()
+
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.popup import Popup
+from kivy.properties import ObjectProperty
+
+
+class MainWidget(GridLayout):
+    lbl_top = ObjectProperty()
+    btn_bottom = ObjectProperty()
+
+
+class PopText(Popup):
+    pass
+
+
+class MyButton(AnchorLayout):
+    my_btn = ObjectProperty()
+
+class MyLabel(AnchorLayout):
+
+    my_lbl = ObjectProperty()
+
+
+class TestApp(App):
+    title = "Changing button text with popup text input Kivy"
+
+    def build(self):
+        return MainWidget()
+
+
+if __name__ == "__main__":
+    TestApp().run()
