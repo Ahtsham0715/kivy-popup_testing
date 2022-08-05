ad524f1e719eaec9e40bf9f2b19f4cec466d7179
Ahtsham0715 <ahtsham50743@gmail.com>
2022-08-05 17:18:04 +0500

    fixes

--- popup_testing.py
+++ popup_testing.py
@@ -14,20 +14,20 @@ class MyLayout(Widget):
     def btn():
         show_popup()
 
-    def label_overwrite( overwrite):
-        print(f'text: {overwrite}')
-        # self.ids.label_1.text = overwrite
-        # self.ids['label_1'].text = str(overwrite)
-        show = MyLayout()
-        show.ids.label_1.text = overwrite
+    # def label_overwrite( overwrite):
+    #     print(f'text: {overwrite}')
+    #     # self.ids.label_1.text = overwrite
+    #     # self.ids['label_1'].text = str(overwrite)
+    #     show = MyLayout()
+    #     show.ids.label_1.text = overwrite
         
 
 class P(FloatLayout):
-
-    def press_button(self):
-        print('Button pressed')
-        text = self.ids.input_1.text
-        MyLayout.label_overwrite(text)
+    pass
+    # def press_button(self):
+    #     print('Button pressed')
+    #     text = self.ids.input_1.text
+    #     MyLayout.label_overwrite(text)
         # self.dismiss()
 
 class MyApp(App):
@@ -41,41 +41,3 @@ def show_popup():
 
 if __name__ == '__main__':
     MyApp().run()
-
-# from kivy.app import App
-# from kivy.uix.gridlayout import GridLayout
-# from kivy.uix.anchorlayout import AnchorLayout
-# from kivy.uix.popup import Popup
-# from kivy.properties import ObjectProperty
-
-
-# class MainWidget(GridLayout):
-#     lbl_top = ObjectProperty()
-#     btn_bottom = ObjectProperty()
-
-
-# class PopText(Popup):
-#     # pass
-#     # text_input = ObjectProperty()
-#     def label_overwrite(self):
-#         print(self.ids.myinput.text)
-#         self.app.root.ids.lbl_top.ids.my_lbl.text = self.ids.myinput.text
-
-
-# class MyButton(AnchorLayout):
-#     my_btn = ObjectProperty()
-
-# class MyLabel(AnchorLayout):
-
-#     my_lbl = ObjectProperty()
-
-
-# class TestApp(App):
-#     title = "Changing button text with popup text input Kivy"
-
-#     def build(self):
-#         return MainWidget()
-
-
-# if __name__ == "__main__":
-#     TestApp().run()
